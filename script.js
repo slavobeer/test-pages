16b04b527983f7f3c39eb813e4da6fc4c218bde2
slavobeer <slavo902@gmail.com>
2026-07-04 22:34:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API = "SEM_VLOŽTE_URL_APPS_SCRIPT";
+const API = "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnTEU99vPBJldaPGMF2ajWH0Rw4VL8XDXKrbOA5lD04YqJ6tBK9A992oCDgt2Mkco12auOvX_XI8CaZkweR0gguNaYLg9EzBmyHP2RjjiUks9bB0OpHY45KQ1kCupHOaq34M7X38XpH0H-yjEvwiMhVX6y-vBLr2LL1NDcsU-CmNYfOw9_zcKBbUlh1uLwBWv5JYyNnRmvmlq4HZdJOT--fw1GgBNZDirH2ZAPm2P14IIK2xTHZSYXkEVf-PjUtow_cjNhjR1MbfLU8xV7mkTlVTEckuJg&lib=M-hGg8H9SiNZie1UYF9RmXGxEtUEwoZJ_";
  async function loadGifts(){
 
     const response = await fetch(API);

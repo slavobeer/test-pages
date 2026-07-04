41ef5dd749dc164ae227dd9ef77414853efde4e1
slavobeer <slavo902@gmail.com>
2026-07-04 22:47:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API = "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnTEU99vPBJldaPGMF2ajWH0Rw4VL8XDXKrbOA5lD04YqJ6tBK9A992oCDgt2Mkco12auOvX_XI8CaZkweR0gguNaYLg9EzBmyHP2RjjiUks9bB0OpHY45KQ1kCupHOaq34M7X38XpH0H-yjEvwiMhVX6y-vBLr2LL1NDcsU-CmNYfOw9_zcKBbUlh1uLwBWv5JYyNnRmvmlq4HZdJOT--fw1GgBNZDirH2ZAPm2P14IIK2xTHZSYXkEVf-PjUtow_cjNhjR1MbfLU8xV7mkTlVTEckuJg&lib=M-hGg8H9SiNZie1UYF9RmXGxEtUEwoZJ_";
+const API = "https://script.google.com/macros/s/AKfycbzbhMR8R9wguY4qnYxa52KPdQ53eIxMZy-35gW4RfW6mBtiRd5rOUTUPIUbzD6lYJBj/exec";
  async function loadGifts(){
 
     const response = await fetch(API);
@@ -9,25 +9,25 @@ const API = "https://script.googleusercontent.com/macros/echo?user_content_key=A
 
     container.innerHTML = "";
 
-    gifts.forEach(gift=>{
+    gifts.forEach(gift => {
 
-        container.innerHTML += `
-        <div class="gift">
+    container.innerHTML += `
+    <div class="gift">
 
-            <h3>${gift.dar}</h3>
+        <h3>${gift.dar}</h3>
 
-            <p>${gift.popis}</p>
+        <p>${gift.popis}</p>
 
-            ${
-                gift.rezervovane
+        ${
+            gift.rezervovane
                 ? "<b>Rezervované</b>"
-                : "<button onclick="reserve(${gift.id})">Rezervovať</button>"
-            }
+                : `<button onclick="reserve(${gift.id})">Rezervovať</button>`
+        }
 
-        </div>
-        `;
+    </div>
+    `;
 
-    });
+});
 
 }
 
